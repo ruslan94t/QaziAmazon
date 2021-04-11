2029e0d40d5cd87a1bc2885641837aba280bef2e
ruslan94t <p3triri@yandex.ru>
2021-04-11 12:15:42 +0300

    add-react-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
+      <Router>
     <div className="app">
+      <Switch>
+        <Route path="/checkout">
+        <h2>Checkout Page</h2>
+        </Route>
+        <Route path="/login">
+        <h2>Login Paga</h2>
+        </Route>
+        <Route path="/">
+          <h2>Home Page</h2>
+        </Route>
+      </Switch>
 
-    </div>
+      </div>
+</Router>
+
   );
 }
 
